e0a36eadf84632c079796aa10eab95cfcf0f3c12
EC2 Default User <ec2-user@ip-172-31-88-222.ec2.internal>
2018-11-17 16:48:35 +0000

    レスポンシブ完了

---
+++ jquery/init.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $("#ham-menu").click(function(){
+    $(".sp_menu_toggle").slideToggle();
+  });
+  
+  $(function () {
+ var headerHight = 100; //ヘッダの高さ
+ $('a[href^=#]').click(function(){
+     var href= $(this).attr("href");
+       var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top-headerHight; //ヘッダの高さ分位置をずらす
+     $("html, body").animate({scrollTop:position}, 550, "swing");
+        return false;
+   });
+  });
+});
